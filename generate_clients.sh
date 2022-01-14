f5da929ad162253cf82d9089702a56462a3d1fc0
Filipe Fraqueiro <59435851+FilipeFraqueiro@users.noreply.github.com>
2022-01-14 17:07:34 +0000

    Create generate_clients.sh

---
+++ generate_clients.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+server_ip="192.168.1.4"
+client_name=$1
+ip_last_octet=$2
+
+privatekey=$( wg genkey )
+publickey=$( echo $privatekey | wg pubkey )
+
+conf_file="/etc/wireguard/wg0_$client_name.conf"
+rm $conf_file
+touch $conf_file
+
+echo "[Interface]" >> $conf_file
+echo "Address = 192.168.8.$ip_last_octet/24" >> $conf_file
+echo "ListenPort = 51820" >> $conf_file
+echo "PrivateKey = $privatekey" >> $conf_file
+echo "PublicKey = $publickey" >> $conf_file
+echo "DNS = 192.168.8.1" >> $conf_file
+echo "" >> $conf_file
+echo "[Peer]" >> $conf_file
+echo "Endpoint = $server_ip:51820" >> $conf_file
+echo "PublicKey = $( cat /etc/wireguard/pubkey )" >> $conf_file
+echo "AllowedIPs = 0.0.0.0/0" >> $conf_file
+
+
+echo "[+] Generated file: $conf_file"
+echo "[+] Client IP 192.168.8.$ip_last_octet"
